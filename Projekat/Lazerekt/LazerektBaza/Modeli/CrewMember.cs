0fa05cc537a44ef203c211356e90292652efbb5e
Igor Vasiljevic <gile@hotmail.com>
2016-04-25 19:46:58 +0200

    Dodani modeli za bazu podataka

---
+++ Projekat/Lazerekt/LazerektBaza/Modeli/CrewMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazerekt.LazerektBaza.Modeli
+{
+    class CrewMember
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int cmid { get; set; }
+        public int uid { get; set; }
+        public string name { get; set; }
+        public int range { get; set; }
+        public double healthPoints { get; set; }
+        public int actionPoints { get; set; }
+        public double attack { get; set; }
+        public double armour { get; set; }
+    }
+}
