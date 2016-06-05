fa99f84911a2b58bd9158d2baadd54ebce072a69
MigrenusMaximus <fedja.tica@gmail.com>
2016-06-05 21:44:16 +0200

    Dodao LoginPage

--- Projekat/Lazerekt/Pages/LoginPage.xaml.cs
+++ Projekat/Lazerekt/Pages/LoginPage.xaml.cs
@@ -13,22 +13,19 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Lazerekt
-{
+namespace Lazerekt {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class LoginPage : Page {
+        public LoginPage() {
             this.InitializeComponent();
         }
 
         private void btnPlay_Click(object sender, RoutedEventArgs e) {
-            this.Frame.Navigate(typeof(Pregame));
+
         }
     }
 }
